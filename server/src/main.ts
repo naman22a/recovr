dd6d1418f173dd20f1f98b835bf5f264cfb399a6
Naman Arora <namanarora1022@gmail.com>
2026-09-03 08:33:03 +0530

    feat: add cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,7 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 declare global {
     namespace Express {
@@ -10,7 +12,12 @@ declare global {
 }
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, { rawBody: true });
+    const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+        rawBody: true,
+    });
+    app.enableCors({
+        origin: [process.env.CLIENT_URL!],
+    });
     await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
